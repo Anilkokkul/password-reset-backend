f930ab0e9b74536aeeed3816e73c512a18284a42
Anilkokkul <anilkokkul1997@gmail.com>
2023-11-25 22:56:31 +0530

    Update allowed origins to include the production URL

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ db();
 
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000", "http://localhost:4000"],
     credentials: true,
     methods: ["GET", "POS", "DELETE", "PUT"],
   })
